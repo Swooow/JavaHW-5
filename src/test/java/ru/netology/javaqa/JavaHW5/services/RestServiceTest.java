71a82c03089a17270604223e6f6f320ac355264d
Swow <stepan2235@gmail.com>
2023-06-07 00:30:18 +0300

    ParametrizedTest commit

--- src/test/java/ru/netology/javaqa/JavaHW5/services/RestServiceTest.java
+++ src/test/java/ru/netology/javaqa/JavaHW5/services/RestServiceTest.java
@@ -1,21 +1,23 @@
 package ru.netology.javaqa.JavaHW5.services;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestServiceTest {
 
-    @Test
-    public void testRestChipCalculate() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/relax.csv")
+    public void testRestChipCalculate(int extended, int income, int expenses, int threshold) {
         RestService service = new RestService();
 
-        int extended = 3;
-        int actual = service.calcRest(10_000, 3_000, 20_000);
+        int actual = service.calcRest(income, expenses, threshold);
 
         Assertions.assertEquals(extended, actual);
     }
 
-    @Test
+    /*@Test
     public void testRestRichCalculate() {
         RestService service = new RestService();
 
@@ -23,5 +25,5 @@ public class RestServiceTest {
         int actual = service.calcRest(100_000, 60_000, 150_000);
 
         Assertions.assertEquals(extended, actual);
-    }
+    }*/
 }
